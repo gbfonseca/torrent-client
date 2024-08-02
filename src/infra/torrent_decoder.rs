f2bd87cf31beb39624f40bbd8a5a08d6db3a4450
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2024-08-01 22:10:07 -0300

    feat: build tracker url

--- src/infra/torrent_decoder.rs
+++ src/infra/torrent_decoder.rs
@@ -1,6 +1,6 @@
 use std::{fs, path::PathBuf};
 
-use crate::models::torrent::Torrent;
+use crate::models::torrent::{Torrent, TorrentFile};
 
 #[allow(dead_code)]
 pub struct TorrentDecoder {}
@@ -13,10 +13,25 @@ impl TorrentDecoder {
 
     pub fn parser(&self, file_path: PathBuf) -> Torrent {
         let content = fs::read(file_path).unwrap();
+        serde_bencode::from_bytes(&content).unwrap()
+    }
 
-        let torrent: Torrent = serde_bencode::from_bytes(&content).unwrap();
+    pub fn build_tracker_url(&self, torrent: TorrentFile) -> String {
+        let url = torrent.announce;
+        // Temporary
+        let peer_id = String::from("12312312312");
 
-        torrent
+        format!(
+            "{}?info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&compact={}&left={}",
+            url,
+            String::from_utf8_lossy(&torrent.info_hash),
+            peer_id,
+            8080,
+            0,
+            0,
+            1,
+            0
+        )
     }
 }
 
@@ -26,14 +41,25 @@ mod tests {
 
     #[test]
     fn should_decode_torrent_file() {
-        let file_path = PathBuf::new().join("archlinux-2024.07.01-x86_64.iso.torrent");
+        let file_path = PathBuf::new().join("debian-12.6.0-amd64-DVD-1.iso.torrent");
         let torrent_decoder = TorrentDecoder::new();
         let torrent = torrent_decoder.parser(file_path);
 
         assert_eq!(
             torrent.info.name,
-            "archlinux-2024.07.01-x86_64.iso".to_string()
+            "debian-12.6.0-amd64-DVD-1.iso".to_string()
         );
-        assert_eq!(torrent.info.piece_length, 524288);
+        assert_eq!(torrent.info.piece_length, 262144);
+    }
+
+    #[test]
+    fn should_build_tracker_url() {
+        let file_path = PathBuf::new().join("debian-12.6.0-amd64-DVD-1.iso.torrent");
+        let torrent_decoder = TorrentDecoder::new();
+        let torrent = torrent_decoder.parser(file_path);
+        let torrent_file = torrent.to_torrent_file();
+        let tracker_url = torrent_decoder.build_tracker_url(torrent_file);
+
+        assert_eq!(tracker_url, "http://bttracker.debian.org:6969/announce?info_hash=2a15945f14b568bd641d54c07abf0b4a8957386d&peer_id=12312312312&port=8080&uploaded=0&downloaded=0&compact=1&left=0".to_string());
     }
 }
--- src/models/torrent.rs
+++ src/models/torrent.rs
@@ -1,12 +1,13 @@
 use serde_bytes::ByteBuf;
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
+use sha1::{Digest, Sha1};
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Node(String, i64);
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct File {
     pub path: Vec<String>,
     pub length: i64,
@@ -15,7 +16,7 @@ pub struct File {
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Info {
     pub name: String,
     pub pieces: ByteBuf,
@@ -37,7 +38,7 @@ pub struct Info {
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Torrent {
     pub info: Info,
     #[serde(default)]
@@ -59,4 +60,35 @@ pub struct Torrent {
     #[serde(default)]
     #[serde(rename = "created by")]
     pub created_by: Option<String>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TorrentFile {
+    pub announce: String,
+
+    pub info_hash: Vec<u8>,
+
+    pub piece_hashes: Vec<Vec<u8>>,
+
+    pub piece_length: i64,
+
+    pub name: String,
+}
+
+#[allow(dead_code)]
+impl Torrent {
+    pub fn to_torrent_file(&self) -> TorrentFile {
+        let mut hasher = Sha1::new();
+        hasher.update(format!("{:?}", self.info));
+        let info_hash = format!("{:x}", hasher.finalize());
+
+        TorrentFile {
+            announce: self.announce.clone().unwrap_or("".into()),
+            name: self.info.name.clone(),
+            piece_length: self.info.piece_length,
+            info_hash: info_hash.as_bytes().into(),
+            piece_hashes: vec![vec![]],
+        }
+    }
 }
